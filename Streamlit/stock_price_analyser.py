cf9902af2b813a8681094a8540f9ff1b44d3a0b2
Sunny Panchal <95635677+28101991SUNNY@users.noreply.github.com>
2023-02-17 19:49:12 -0500

    Update stock_price_analyser.py

--- Streamlit/stock_price_analyser.py
+++ Streamlit/stock_price_analyser.py
@@ -19,15 +19,15 @@ ticker_symbol = st.text_input(
         "AAPL",
         key = "placeholder"
  )
+ticker_data = yf.Ticker(ticker_symbol)
+
+
 
 col1,col2 = st.columns(2)
-
 with col1:
     Start_date = st.date_input("Input Start Date: ", datetime.date(2015,1,1))  # start date
 with col2:
     End_date = st.date_input("Input End Date: ", datetime.date(2023,1,1))  # End date
-
-ticker_data = yf.Ticker(ticker_symbol)
 
 ticker_df = ticker_data.history(
                                 period="1d", 
@@ -36,15 +36,18 @@ ticker_df = ticker_data.history(
                                 end= End_date,
                                 )
 
+
 st.write(f"""
     ### {ticker_symbol} stock price info
 """)
 
 st.write(ticker_df)
 
+
+
+
+
 col1, col2 = st.columns(2)
-
-
 ## showcasing the line chart
 with col1:
     st.write(""" ### Daily closing Prices""")
@@ -53,4 +56,3 @@ with col2:
     st.write(""" ### Daily Volume""")
     st.line_chart(ticker_df.Volume)
 
-    
